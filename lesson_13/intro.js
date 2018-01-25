5a0ec36fbd61827df14ef0f9ae61ab2c8b2155e2
pseudoyim <paul.j.yim@gmail.com>
2018-01-24 22:37:34 -0600

    Added lesson_14

---
+++ lesson_13/intro.js
@@ -0,0 +1,8 @@
+/*
+'undefined' is the default return value on the console when nothing is explicitly returned using the 'return' keyword.
+*/
+
+function sayHello(){
+	var message = "Hello!";
+	console.log(message);
+}
