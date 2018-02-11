a4fe28391d388c49202197a00b3e07b104fb7eeb
pseudoyim <paul.j.yim@gmail.com>
2018-02-10 23:41:50 -0600

    Added lesson_19 work.

--- lesson_19/toggle_class.js
+++ lesson_19/toggle_class.js
@@ -8,3 +8,26 @@ var featuredArticle;
 featuredArticle = $('.featured')
 
 featuredArticle.toggleClass('featured') //just pass in the name of the class you want to toggle.
+
+
+
+
+
+
+
+/*
+For this quiz, remove the class 'featured' from Article #2 and add it to Article #3!
+
+You must use jQuery's toggleClass method!
+*/
+
+// don't change these variable!
+var article2, article3;
+
+article2 = $('li.featured')
+
+article2.toggleClass('featured')
+
+article3 = article2.next()
+
+article3.toggleClass('featured')
