784183848c05bc853cbe9fbc7cb022394b89c332
pseudoyim <paul.j.yim@gmail.com>
2018-02-13 23:00:20 -0600

    Added lesson_20 work

---
+++ lesson_20/event_object.js
@@ -0,0 +1,26 @@
+// The evt.target property can be used to access just the clicked on element! 
+//Example:
+$( 'article' ).on( 'click', function( evt ) {
+    $( evt.target ).css( 'background', 'red' );
+});
+
+
+// Use case 1: Prevent a default action from occurring.
+$( '#myAnchor' ).on( 'click', function( evt ) {
+    evt.preventDefault();
+    console.log( 'You clicked a link!' );
+});
+//Prevents the link from opening when the user clicks on it.
+
+
+// Use case 2:
+event.keyCode
+// to learn what key was pressed - invaluable if you need to listen for a specific key
+
+// Use case 3:
+event.pageX & event.pageY
+// to know where on the page the click occurred - helpful for analytics tracking
+
+// Use case 4:
+event.type
+// to find what event happened - useful if listening to a target for multiple events
