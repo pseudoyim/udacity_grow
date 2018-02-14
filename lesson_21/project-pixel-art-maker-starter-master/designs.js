1e1f317b6daa94cf9f1b4f1282eb36d6bdb76ba0
pseudoyim <paul.j.yim@gmail.com>
2018-02-14 08:58:33 -0600

    Added lesson_21 work

--- lesson_21/project-pixel-art-maker-starter-master/designs.js
+++ lesson_21/project-pixel-art-maker-starter-master/designs.js
@@ -1,5 +1,13 @@
 // Select color input
+var color = $('#colorPicker');
+
 // Select size input
+var height, width;
+
+$('#submitSize').on('click', function(){
+	height = $('#inputHeight');
+	width = $('#inputWidth');
+});
 
 // When size is submitted by the user, call makeGrid()
 
