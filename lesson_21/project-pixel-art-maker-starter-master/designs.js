314f8b2254f47b5662d9d7c80c319c533c21bc54
pseudoyim <paul.j.yim@gmail.com>
2018-03-02 23:41:06 -0600

    Updated lesson_21 work

--- lesson_21/project-pixel-art-maker-starter-master/designs.js
+++ lesson_21/project-pixel-art-maker-starter-master/designs.js
@@ -7,18 +7,22 @@ var height = Number($('#inputHeight').val());
 var width = Number($('#inputWidth').val());
 var table = $('#pixelCanvas')
 
-var height, weight, table;
+// var height, weight, table;
 
 
-//WORKS!
 // When size is submitted by the user, call makeGrid()
 $('#submitSize').click(function(){
 	$('#pixelCanvas').children().remove();
 	var height = new Number($('#inputHeight').val());
 	for (var i = 0; i < height; i++) {
-		// $('#pixelCanvas').append('<tr><td></td></tr>');  this works by itself
-		$('#pixelCanvas').append('<tr></tr>');
+		// $('#pixelCanvas').append('<tr><td></td></tr>');  //this works by itself
+	
+		$('#pixelCanvas').append('<tr><td></td></tr>');
+		for (var i = 0; i < width; i++) {
+			alert('boom!');
+			// $('tr').append('<td></td>');
 		//insert however many <td>s here.
+		}
 	}
 });
 
