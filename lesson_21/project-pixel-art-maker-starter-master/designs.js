0e768391fc85da9e88c9c4e6bf6db3d9b6a42b6d
pseudoyim <paul.j.yim@gmail.com>
2018-03-04 23:53:27 -0600

    makeGrid function

--- lesson_21/project-pixel-art-maker-starter-master/designs.js
+++ lesson_21/project-pixel-art-maker-starter-master/designs.js
@@ -1,24 +1,18 @@
 // Select color input
-// var color = $('#colorPicker').val();
+var color = $('#colorPicker').val();
 
 // alert('working');
 // console.log('hello');
 
-// var height, weight, table;
 // Select size input
 var rows = Number($('#inputHeight').val());
 var cols = Number($('#inputWidth').val());
 var table = $('table');
 
-// When size is submitted by the user, call makeGrid()
-$('#submitSize').click(function(){
 
+function makeGrid(){
 	$('table').children().remove();
-	// var height = new Number($('#inputHeight').val());
 
-	alert('button clicked')
-
-	// Select size input
 	var rows = Number($('#inputHeight').val());
 	var cols = Number($('#inputWidth').val());
 	var table = $('table');
@@ -27,10 +21,13 @@ $('#submitSize').click(function(){
 	    var tr = $('<tr></tr>');
 
 	    for (var c = 0; c < cols; c++){
-	        $('<td>some value</td>').appendTo(tr); //fill in your cells with something meaningful here
+	        $('<td></td>').appendTo(tr); //fill in your cells with something meaningful here
 			console.log(c)	    
 	    tr.appendTo(table);
 	    console.log(r)
 	    }
 	}
-});
+};
+
+// When size is submitted by the user, call makeGrid()
+$('#submitSize').click(makeGrid);
